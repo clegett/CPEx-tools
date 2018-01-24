15873653ba7d19521b29ba84cd74a2b5b0d3b6bf
clegett <carey.legett@stonybrok.edu>
2018-01-24 15:40:26 -0500

    formatting and continuum removal skeleton in continum_removal.py

--- continuum_removal.py
+++ continuum_removal.py
@@ -22,17 +22,17 @@ parser = argparse.ArgumentParser(prog='continuum_removal.py',
         normalizes all y data such that the linear fit line is at a value
         of 1.''', epilog='''For more information, contact Chip at
         carey.legett@stonybrook.edu.''')
-parser.add_argument('-f', '--input-file', nargs=1, required=True, help='''The
+parser.add_argument('-f', '--input-file', required=True, help='''The
         name of the input file (include path if the file is not in this
         directory)''', dest='ifile')
 parser.add_argument('-o', '--output-file', nargs='?', default='output.txt', 
         help='''The name of the output file (include path if you want the file
         somewhere other than in the current directory). Defaults to 
         output.txt''', dest='ofile')
-parser.add_argument('-a', '--xstart', type=float, nargs=1, required=True,
+parser.add_argument('-a', '--xstart', type=int, required=True,
         help='The x value at which to begin the continuum calculation.', 
         dest='xa')
-parser.add_argument('-b', '--xstop', nargs=1, type=float, required=True,
+parser.add_argument('-b', '--xstop', type=int, required=True,
         help='The x value at which to end the continuum calculation.', 
         dest='xb')
 parser.add_argument('-d', '--delimiter', nargs='?', default=',', help='''The
@@ -44,23 +44,39 @@ parser.add_argument('-n', '--start-line', nargs='?', default=0, help='''Skip
 
 args = parser.parse_args()
 
-#mydialect = csv.register_dialect('asd', delimiter=args.delim, 
-#        skipinitialspace=False)
-
-
-#reader = csv.reader(itertools.islice(f,args.skip,None), dialect='asd')
 try:
-    with open(args.ifile[0], newline='')  as f:
-        rawdata=f.readlines()
+    with open(args.ifile, newline='')  as f:
+        rawdata=itertools.islice(f.readlines(),args.skip,None)
         f.close()
     if not rawdata:
-        sys.exit(('No data in file ' + f)
+        sys.exit('No data in file ' + f)
 except IOError as e:
     sys.exit('I/O error: file {}: {}'.format(args.ifile[0], e))
 except:
-    print('Unexpected error:', sys.exc_info()[0]
+    sys.exit('Unexpected error: {}'.format(sys.exc_info()[0]))
 
 strippeddata = map(str.strip, rawdata)
-data = [line.split() for line in strippeddata]
+strdata = [line.split() for line in strippeddata]
 
-print(data)
+try:
+    data = [[float(astring) for astring in inner] for inner in strdata]
+except:
+    sys.exit('''Converting strings to floats failed. Check input data. Error:
+    {}'''.format(sys.exc_info()[0]))
+
+
+startx=float(args.xa)
+stopx=float(args.xb)
+starty=data[args.xa-int(data[0][0])][1]
+stopy=data[args.xb-int(data[0][0])][1]
+
+slope=(stopy-starty)/(stopx-startx)
+intercept=starty-(slope*startx)
+
+newdata = list()
+
+for i in range(len(data)):
+    continuum=slope*data[i][1]+intercept
+    newdata[i][0]=data[i][0]
+    newdata[i][1]=data[i][1]/continuum
+    print(data[i] + newdata[i])
