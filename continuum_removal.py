e55cb3cef39deacc55dae31d6c5ff8da3409f719
clegett <carey.legett@stonybrok.edu>
2018-01-24 11:13:44 -0500

    continuum_removal.py formatting

--- continuum_removal.py
+++ continuum_removal.py
@@ -17,31 +17,36 @@ __email__ = "carey.legett@stonybrook.edu"
 __status__ = "Development"
 
 parser = argparse.ArgumentParser(prog="continuum_removal.py",
-            description='''This program takes a delimited text file containing
-            x and y values and fits a line from y(xstart) to y(xstop) and
-            normalizes all y data such that the linear fit line is at a value
-            of 1.''', epilog='''For more information, contact Chip at
-            carey.legett@stonybrook.edu.''')
-parser.add_argument('-f', '--input-file', nargs=1, required=True, help='''The 
-    name of the input file (include path if the file is not in this directory
-    )''', dest='ifile')
+        description='''This program takes a delimited text file containing
+        x and y values and fits a line from y(xstart) to y(xstop) and
+        normalizes all y data such that the linear fit line is at a value
+        of 1.''', epilog='''For more information, contact Chip at
+        carey.legett@stonybrook.edu.''')
+parser.add_argument('-f', '--input-file', nargs=1, required=True, help='''The
+        name of the input file (include path if the file is not in this
+        directory)''', dest='ifile')
 parser.add_argument('-o', '--output-file', nargs='?', default='output.txt', 
-    help='''The name of the output file (include path if you want the file 
-    somewhere other than in the current directory). Defaults to output.txt''',
-    dest='ofile')
+        help='''The name of the output file (include path if you want the file
+        somewhere other than in the current directory). Defaults to 
+        output.txt''', dest='ofile')
 parser.add_argument('-a', '--xstart', type=float, nargs=1, required=True,
-    help='The x value at which to begin the continuum calculation.', dest='xa')
+        help='The x value at which to begin the continuum calculation.', 
+        dest='xa')
 parser.add_argument('-b', '--xstop', nargs=1, type=float, required=True,
-    help='The x value at which to end the continuum calculation.', dest='xb')
+        help='The x value at which to end the continuum calculation.', 
+        dest='xb')
 parser.add_argument('-d', '--delimiter', nargs='?', default=',', help='''The
-delimiter for the input and output files. Defaults to \',\' ''', dest='delim')
+        delimiter for the input and output files. Defaults to \',\' ''', 
+        dest='delim')
 parser.add_argument('-n', '--start-line', nargs='?', default=0, help='''Skip
-    this many lines of the input file before reading in data.''', type=int,
-    dest='skip')
+        this many lines of the input file before reading in data.''', type=int,
+        dest='skip')
 
 args = parser.parse_args()
-print(args.delim)
-mydialect = csv.register_dialect('asd', delimiter=args.delim, skipinitialspace=False)
+
+mydialect = csv.register_dialect('asd', delimiter=args.delim, 
+        skipinitialspace=False)
+
 with open(args.ifile[0], newline='')  as f:
     reader = csv.reader(itertools.islice(f,args.skip,None), dialect='asd')
     try:
