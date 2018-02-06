6db7a7d764894203525eb9be22dfda74b7ab3df5
clegett <carey.legett@stonybrok.edu>
2018-02-06 11:02:01 -0500

    added prototype for mstm-run-input.py

---
+++ mstm-run-input.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""Control class for the MSTM Model Manager
+
+"""
+
+import os
+
+__author__ = 'Carey Legett'
+__copyright__ = 'Copyright 2018, Stony Brook University'
+__credits__ = ['Carey Legett']
+
+__version__ = '1.0'
+__maintainer__ = 'Carey Legett'
+__email__ = 'carey.legett@stonybrook.edu'
+__status__ = 'Production'
+
+def read_packlsd():
+    pass
+
+def rescale_packlsd():
+    pass
+
+
