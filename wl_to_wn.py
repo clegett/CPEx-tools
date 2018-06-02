6c4c00ab6224a06969a9caf930da03f976a229e0
clegett <carey.legett@stonybrook.edu>
2018-06-02 14:24:13 -0500

    wl_to_wn functional

--- wl_to_wn.py
+++ wl_to_wn.py
@@ -116,9 +116,10 @@ def read_input_file(filename, delimiter, whitespace_flag, skip_lines, xunits,
         sys.exit('I/O error: file {}: {}'.format(filename, e))
 
     mydata = [[float(astring) for astring in inner] for inner in rawdata]
+    print(mydata)
     if verbose:
         print('Assigning units.')
-    mydata = [row[0] * ureg[f'{xunits}'] for row in mydata]
+    mydata = [[row[0] * ureg[f'{xunits}'], row[1]] for row in mydata]
     # If this fails, it is likely that the data read from the input file is not
     #  numeric. It could also happen if the delimiter was not correctly
     # specified.
@@ -132,7 +133,7 @@ def read_input_file(filename, delimiter, whitespace_flag, skip_lines, xunits,
 def write_to_output(somedata, out_filename, out_delim, verbose_flag):
     if verbose_flag:
         print('Writing output file')
-
+        print(somedata)
     try:
         with open(out_filename, 'w', newline='') as outfile:
             writer = csv.writer(outfile, delimiter=out_delim)
@@ -146,10 +147,11 @@ def write_to_output(somedata, out_filename, out_delim, verbose_flag):
 
 if __name__ == '__main__':
     args = parse_cmdln_args()
-    x = ureg[f'{args.x}']
+    x = ureg[f'{args.xunits}']
     data = read_input_file(args.ifile, args.in_delim, args.whitespace,
                            args.skip, args.xunits, args.v)
 
-    data = [line[0].to(1 / ureg.centimeter, 'sp') for line in data]
+    #print(data)
+    data = [[line[0].to(1 / ureg.centimeter, 'sp').magnitude, line[1]] for line in data]
 
     write_to_output(data, args.ofile, args.out_delim, args.v)
